4e41edaf2c2a732e3e98423369261de1383fb4d9
- <stefanos@cpan.org>
2013-03-03 01:17:52 +0000

    Import of STEFANOS/MIME-Base2-1.1 from CPAN.
    gitpan-cpan-distribution: MIME-Base2
    gitpan-cpan-version:      1.1
    gitpan-cpan-path:         STEFANOS/MIME-Base2-1.1.tar.gz
    gitpan-cpan-author:       STEFANOS
    gitpan-cpan-maturity:     released

--- Base2.pm
+++ Base2.pm
@@ -5,7 +5,7 @@ use strict;
 use warnings;
 
 use vars qw( $VERSION );
-$VERSION = '1.0';
+$VERSION = '1.1';
 
 sub import {
 	*encode = \&encode_base2;
@@ -44,7 +44,7 @@ NOTHING
 
 =head1 AUTHOR
 
-Stefan Gipper <stefanos@cpan.org>, http://www.coder-world.de/
+-
 
 =head1 SEE ALSO
 
